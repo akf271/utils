dfc9da6f0a88335136bca108ebfacf7e5a8ddc66
zhaoy <zhaoy@chemao.com.cn>
2018-07-11 16:22:16 +0800

    git

--- hutool/hutool-cron/src/test/java/cn/hutool/cron/demo/TestJob.java
+++
@@ -1,9 +0,0 @@
-package cn.hutool.cron.demo;
-
-import cn.hutool.core.lang.Console;
-
-public class TestJob{
-	public void doTest() {
-		Console.log("Job running...");
-	}
-}
--- hutool/hutool-extra/src/main/java/cn/hutool/extra/template/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 第三方模板引擎封装
- * 
- * @author looly
- *
- */
-package cn.hutool.extra.template;
